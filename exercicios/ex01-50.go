623f8cd128127a0df123f2ce3cd2e8179139dcc1
rinicius <viniciusrsdede@outlook.com>
2024-02-08 21:18:12 -0300

    feat: update exercises

--- exercicios/ex01-50.go
+++ exercicios/ex01-50.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+)
 
 func ex1() {
 	count := 100
@@ -27,12 +32,10 @@ func ex3() {
 	}
 }
 
-func ex4() bool {
-	var num int
-	fmt.Scan(&num)
+func ex4(num int) bool {
 
 	for i := 2; i*i <= num; i++ {
-		if num%i == 0 {
+		if math.Mod(float64(num), float64(i)) == 0 {
 			return false
 		}
 	}
@@ -69,5 +72,146 @@ func ex5Alt() {
 }
 
 func ex6() {
-	// Fatorial... to be continued :p
+	var num int
+	fmt.Println("Digite o número: ")
+	fmt.Scan(&num)
+
+	for i := num - 1; i > 0; i-- {
+		num *= i
+	}
+
+	fmt.Println("Resultado: ", num)
+}
+
+func ex7() {
+	// 7. Implemente um programa que calcule o máximo divisor comum (MDC) de dois números.
+	var num1 int
+	var num2 int
+
+	fmt.Scan(&num1)
+	fmt.Scan(&num2)
+
+	div := 1
+
+	for div != 0 {
+
+		if num2 > num1 {
+			num1, num2 = num2, num1
+		}
+
+		div = num1 % num2
+		num1 = num1 % num2
+
+	}
+
+	fmt.Println("MDC: ", num2)
+}
+
+func ex8() {
+	// Faça um programa que inverta um número inteiro.
+	var num string
+
+	fmt.Scan(&num)
+
+	numstring := strings.Split(num, "")
+
+	var reverse string
+
+	for i := len(numstring); i > 0; i-- {
+		reverse += numstring[i-1]
+	}
+
+	fmt.Print(reverse)
+
+}
+
+func ex9() {
+	// Escreva um programa que verifique se uma string é um palíndromo.
+	var checkstring string
+
+	fmt.Scan(&checkstring)
+
+	checkstringSplit := strings.Split(checkstring, "")
+
+	var reverse string
+
+	for i := len(checkstringSplit); i > 0; i-- {
+		reverse += checkstringSplit[i-1]
+	}
+
+	if reverse == checkstring {
+		fmt.Println("É palíndromo")
+	} else {
+		fmt.Println("Não é palíndromo")
+	}
+}
+
+func ex10() {
+	// Implemente um programa que converta um número decimal para binário.
+	var num int
+
+	fmt.Scan(&num)
+
+	div := 1
+	leftover := []int{}
+
+	for num != 0 {
+		div = num % 2
+		num /= 2
+		leftover = append(leftover, div)
+	}
+
+	for i, j := 0, len(leftover)-1; i < j; i, j = i+1, j-1 {
+		leftover[i], leftover[j] = leftover[j], leftover[i]
+	}
+
+	fmt.Println(leftover)
+}
+
+func ex11() {
+	// Crie um programa que calcule a média de um conjunto de números.
+	var input string
+
+	println("Insira os números (use o espaço para separar)")
+	fmt.Scan(&input)
+
+	inputSlice := strings.Split(input, " ")
+	fmt.Println(inputSlice)
+
+	var average int
+
+	for i := 0; i < len(inputSlice); i++ {
+		num, _ := strconv.Atoi(inputSlice[i])
+		average += num
+	}
+
+	average = average / len(inputSlice)
+
+	println(average)
+}
+
+func ex12() bool {
+	var num int
+
+	fmt.Scan(&num)
+
+	sum := 1
+
+	for i := 2; i <= int(math.Sqrt(float64(num))); i++ {
+		if num%i == 0 {
+			sum += i
+
+			if i != num/i {
+				println(num/i, i)
+				sum += num / i
+			}
+		}
+	}
+
+	if sum == num {
+		return true
+	} else {
+		return false
+	}
+
 }
--- exercicios/main.go
+++ exercicios/main.go
@@ -1,15 +1,5 @@
 package main
 
 func main() {
-	// ex1_basics()
-	// ex2_basics()
-	// fmt.Println(ex3_basics("vinicius"))
-	// ex4_basics()
-	// ex1()
-	// ex2()
-	// ex3()
-	// fmt.Print(ex4())
-	// ex5()
-	// ex5Alt()
-	ex6()
+	println(ex12())
 }
